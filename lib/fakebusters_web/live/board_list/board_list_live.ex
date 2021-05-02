350e9a8700a7c5c103ec697c12620b85c46352e4
AnicetNgrt <anicet.nougaret@zohomail.eu>
2021-05-02 18:59:49 +0200

    added boards search filters

--- lib/fakebusters_web/live/board_list/board_list_live.ex
+++ lib/fakebusters_web/live/board_list/board_list_live.ex
@@ -9,7 +9,7 @@ defmodule FakebustersWeb.BoardListLive do
   alias FakebustersWeb.LiveComponents.BoardPreview
 
   @impl true
-  def mount(_params, %{"modes" => modes} = session, socket) do
+  def mount(_params, session, socket) do
     user =
       if Map.has_key?(session, "user_token") do
         Accounts.get_user_by_session_token(session["user_token"])
@@ -19,9 +19,15 @@ defmodule FakebustersWeb.BoardListLive do
 
     socket =
       socket
-      |> assign(:search, "")
-      |> assign(:modes, modes)
+      |> assign(:filter_search, "")
+      |> assign(:filter_membership, "any")
+      |> assign(:filter_topic_id, nil)
+      |> assign(:filter_state, "any")
       |> assign(:current_user, user)
+      |> assign(
+        :topics,
+        Enum.map(Topics.list_topics_sort_names_asc(), &{&1.name, &1.id})
+      )
       |> refresh_list()
 
     Boards.subscribe_globally()
@@ -35,10 +41,26 @@ defmodule FakebustersWeb.BoardListLive do
   end
 
   @impl true
-  def handle_event("filter", %{"filter" => %{"search_phrase" => search}}, socket) do
+  def handle_event("filter",
+    %{"filter" => %{
+      "search_phrase" => search,
+      "state" => state,
+      "membership" => membership,
+      "topic_id" => topic_id
+    } = filter
+    }, socket
+  ) do
+    topic_id = case Integer.parse(topic_id, 10) do
+      {id, _} -> id
+      _ -> nil
+    end
+
     socket =
       socket
-      |> assign(:search, search)
+      |> assign(:filter_search, search)
+      |> assign(:filter_membership, membership)
+      |> assign(:filter_topic_id, topic_id)
+      |> assign(:filter_state, state)
       |> refresh_list()
 
     {:noreply, socket}
@@ -46,7 +68,10 @@ defmodule FakebustersWeb.BoardListLive do
 
   defp refresh_list(socket) do
     user = socket.assigns[:current_user]
-    search = socket.assigns[:search]
+    search = socket.assigns[:filter_search]
+    membership = socket.assigns[:filter_membership]
+    topic_id = socket.assigns[:filter_topic_id]
+    state = socket.assigns[:filter_state]
 
     boards = if String.length(search) > 1 do
       Boards.search_boards(search)
@@ -57,6 +82,15 @@ defmodule FakebustersWeb.BoardListLive do
     |> Enum.map(&Map.put(&1, :topic_name, Topics.get_topic!(&1.topic_id).name))
     |> Enum.map(&Map.put(&1, :is_member, Boards.is_member?(&1, user)))
     |> Enum.map(&Map.put(&1, :judge, Boards.judge(&1)))
+    |> Enum.filter(&(topic_id == nil or &1.topic_id == topic_id))
+    |> Enum.filter(
+      &((membership == "member" and &1.is_member)
+      or (membership == "outsider" and not &1.is_member)
+      or membership == "any"))
+    |> Enum.filter(
+      &((state == "finished" and &1.finished)
+      or (state == "ongoing" and not &1.finished)
+      or state == "any"))
 
     assign(socket, :boards, boards)
   end
